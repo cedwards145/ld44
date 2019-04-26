9b079a11144af6066d2cd70d9e74f32572893588
Cam <cameronedwards145@gmail.com>
2019-04-26 18:05:33 +0100

    Included JSLint, webpack and set up build scripts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+"use strict";
+
 // Main entry point
+(function () {
+    var lastTimeStamp = 0;
+    function main(currentTime) {
+        window.requestAnimationFrame(main);
+
+        var deltaTime = (currentTime - lastTimeStamp) / 1000;
+
+        console.log("Elapsed (s): " + deltaTime);
+        //update(tFrame);
+        //render();
+        lastTimeStamp = currentTime;
+    }
+
+    main();
+}());
