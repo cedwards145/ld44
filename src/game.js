d7e859c6cd5c39d362ea549a43699425cfbaa637
Cam <cameronedwards145@gmail.com>
2019-04-28 08:59:00 +0100

    Removed rectangle class and moved intersect logic
    Intersection logic now in gameobject, no need to track
    a separate collisionRect, makes resolution code a bit
    nicer

--- src/game.js
+++ src/game.js
@@ -35,10 +35,6 @@ class Game {
     }
     
     update(deltaTime) {
-        if (deltaTime > (1/60)) {
-            console.warn("Slow frame");
-        }
-
         // Update input, AI, game logic
         for (var i = 0; i < this.gameObjects.length; i++) {
             this.gameObjects[i].update(deltaTime);
--- src/gameobject.js
+++ src/gameobject.js
@@ -1,5 +1,3 @@
-import { Rectangle } from "./rectangle";
-
 class GameObject {
     constructor(x, y, width, height) {
         this.x = x;
@@ -12,20 +10,17 @@ class GameObject {
         this.velocity = 0;
         // Indicates whether object should be collidable
         this.solid = true;
-
-        this.collisionRect = new Rectangle(this.x, this.y, this.width, this.height);
     }
 
     update(deltaTime) {
-        // Keep collision rectangle in sync with object
-        this.updateCollisionRect();
     }
 
-    updateCollisionRect() {
-        this.collisionRect.x = this.x;
-        this.collisionRect.y = this.y;
-        this.collisionRect.width = this.width;
-        this.collisionRect.height = this.height;
+    intersects(other) {
+        var r = !(other.x >= this.x + this.width ||
+                 other.x + other.width <= this.x ||
+                 other.y >= this.y + this.height ||
+                 other.y + other.height <= this.y);
+        return r;
     }
 
     collideWith(other, x, y) {
@@ -33,39 +28,44 @@ class GameObject {
             return;
         }
 
+        // Hack to make collisions less costly. Only check for collisions with 
+        // very near objects (within 4 tiles up or down)
+        if (Math.abs(this.x - other.x) > 64 ||
+            Math.abs(this.y - other.y) > 64) {
+                return;
+        }
+
         // Awful collision resolution
-        if (this.collisionRect.intersects(other.collisionRect)) {
+        if (this.intersects(other)) {
             // Horizontal resolution
             if (x > 0) {
-                if (this.collisionRect.x <= other.collisionRect.x + other.collisionRect.width && 
-                    this.collisionRect.x > other.collisionRect.x) {
+                if (this.x <= other.x + other.width && 
+                    this.x > other.x) {
                     // This on Right side of other
-                    this.x = other.collisionRect.x + other.collisionRect.width;
+                    this.x = other.x + other.width;
                 }
-                else if (this.collisionRect.x + this.collisionRect.width >= other.x &&
-                         this.collisionRect.x + this.collisionRect.width < other.collisionRect.x + other.collisionRect.width) {
+                else if (this.x + this.width >= other.x &&
+                         this.x + this.width < other.x + other.width) {
                     // This on Left side of other
-                    this.x = other.collisionRect.x - this.width;
+                    this.x = other.x - this.width;
                 }
             }
             // Vertical resolution
             else if (y > 0) {
                 
-                if (this.collisionRect.y + this.collisionRect.height >= other.collisionRect.y &&
-                    this.collisionRect.y + this.collisionRect.height < other.collisionRect.y + other.collisionRect.height) {
+                if (this.y + this.height >= other.y &&
+                    this.y + this.height < other.y + other.height) {
                     // This above other
-                    this.y = other.collisionRect.y - this.height;
+                    this.y = other.y - this.height;
                 }
-                else if (this.collisionRect.y <= other.collisionRect.y + other.collisionRect.height &&
-                    this.collisionRect.y > other.collisionRect.y) {
+                else if (this.y <= other.y + other.height &&
+                    this.y > other.y) {
                     // This below other
-                    this.y = other.collisionRect.y + other.collisionRect.height;
+                    this.y = other.y + other.height;
                 }
                 
                 this.velocity = 0;
             }
-
-            this.updateCollisionRect();
         }
     }
 
@@ -76,8 +76,6 @@ class GameObject {
 
         this.velocity += 320 * deltaTime;
         this.y += this.velocity * deltaTime;
-
-        this.updateCollisionRect();
     }
 
     draw(context) { }
